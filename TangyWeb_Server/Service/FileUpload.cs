73b9b49c6817927a1563c642e249aff8df11d790
Tienvan2112 <55881057+LYVANTIEN@users.noreply.github.com>
2024-03-18 07:45:37 +0700

    update all

--- TangyWeb_Server/Service/FileUpload.cs
+++ TangyWeb_Server/Service/FileUpload.cs
@@ -19,7 +19,7 @@ namespace TangyWeb_Server.Service
             }
             return false;
         }
-//ádjalkd new my dđsdấdasdas fdfdfdfdfdf new , update order suprqqweaasweqwe yuyiopeeeeeeeeeeqewqweádadasda up to
+//ádjalkd new my dđsdấdasdas fdfdfdfdfdf new , update order suprqqweaasweqwe yuyiopeeeeeeeeeeqewqweádadasda up to new
         public async Task<string> UploadFile(IBrowserFile file)
         {
             FileInfo fileInfo = new FileInfo(file.Name);
